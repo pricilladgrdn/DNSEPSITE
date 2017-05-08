3560c7e4c668121415ead20131234310eefb06d8
Eliott <eliott.benoit4@gmail.com>
2017-05-08 18:44:18 +0200

    add rotations reflexion room

--- js/room2.js
+++ js/room2.js
@@ -1,8 +1,56 @@
+function getRotationDegrees(obj) {
+    var matrix = obj.css("-webkit-transform") ||
+    obj.css("-moz-transform")    ||
+    obj.css("-ms-transform")     ||
+    obj.css("-o-transform")      ||
+    obj.css("transform");
+    if(matrix !== 'none') {
+        var values = matrix.split('(')[1].split(')')[0].split(',');
+        var a = values[0];
+        var b = values[1];
+        var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
+    } else { var angle = 0; }
+    return (angle < 0) ? angle + 360 : angle;
+}
+
+// http://stackoverflow.com/questions/3276467/adjusting-div-width-and-height-after-rotated
+jQuery.fn.rotate = function(degrees) {
+	degrees += getRotationDegrees($(this));
+    $(this).css({'-webkit-transform' : 'rotate('+ degrees +'deg)',
+                 '-moz-transform' : 'rotate('+ degrees +'deg)',
+                 '-o-transform' : 'rotate('+ degrees +'deg)',
+                 '-ms-transform' : 'rotate('+ degrees +'deg)',
+                 'transform' : 'rotate('+ degrees +'deg)'});
+
+    /*var parent = $(this).parent();
+    var h = parent.height();
+    var w = parent.width();
+    var t = $(this).css("top");
+    var l = $(this).css("left");
+
+    parent.height(w);
+    parent.width(h);
+    parent.css("top", t);
+    parent.css("left", l);*/
+
+    return $(this);
+};
+
+function randomize(min, max) {
+	return Math.floor((Math.random() * (max - min + 1)) + min);
+}
 
 $(document).ready(function() {
-	function randomize(min, max) {
-		return Math.floor((Math.random() * (max - min + 1)) + min);
-	}
+
+	var lastImage;
+
+	$(document).keydown(function() {
+	   	if(event.keyCode == 37) {  //prev key
+			lastImage.rotate(-90);
+	    } else if(event.keyCode == 39) {  //next key
+	    	lastImage.rotate(90);
+		}
+	});
 
 	$('ul.images li').each(function() {
 		var w = Math.floor($(this).width() / $(window).width() * 100);
@@ -16,44 +64,8 @@ $(document).ready(function() {
 	if ('ontouchstart' in window) {
 		// target elements with the "draggable" class
 		interact('.draggable')
-		.draggable({
-			// enable inertial throwing
-			inertia: false,
-			// keep the element within the area of it's parent
-			restrict: {
-				restriction: "parent",
-				endOnly: true,
-				elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-			},
-			// enable autoScroll
-			autoScroll: true,
-
-			// call this function on every dragmove event
-			onmove: dragMoveListener,
-			// call this function on every dragend event
-			onend: function (event) {}
-		});
-
-		function dragMoveListener (event) {
-			var target = event.target,
-			    // keep the dragged position in the data-x/data-y attributes
-			    x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-			    y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-
-			// translate the element
-			target.style.webkitTransform =
-			target.style.transform =
-				'translate(' + x + 'px, ' + y + 'px)';
-
-			// update the posiion attributes
-			target.setAttribute('data-x', x);
-			target.setAttribute('data-y', y);
-		}
-
-		// this is used later in the resizing and gesture demos
-		window.dragMoveListener = dragMoveListener;
+		.draggable();
 	} else {
-
 		$('ul.images li').mousedown(function() {
 			$(this).css('z-index', 100).siblings().each(function() {
 				style = window.getComputedStyle($(this)[0]);
@@ -61,6 +73,7 @@ $(document).ready(function() {
 				if(zindex > 0)
 					$(this).css('z-index', zindex - 1);
 			});
+			lastImage = $(this).find("img");
 		}).draggable();
 	}
 });
