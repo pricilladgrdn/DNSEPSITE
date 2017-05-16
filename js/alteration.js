2a517a77709aa3d49b7a1582ee2e3485866f16f2
Prici <you@example.com>
2017-05-16 20:18:42 +0200

    room alteration

--- js/alteration.js
+++ js/alteration.js
@@ -27,3 +27,18 @@ function loadImage ( src, callback ) {
 	};
 	imageEl.src = src;
 }
+
+/* grille images responsive */
+
+function getRandomSize(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+var allImages = "";
+
+for (var i = 0; i < 25; i++) {
+  var width = getRandomSize(200, 400);
+  var height =  getRandomSize(200, 400);
+}
+
+$('#photos').append(allImages);
